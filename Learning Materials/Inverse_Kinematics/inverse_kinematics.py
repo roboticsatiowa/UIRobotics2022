5d8fa7e38a74f799985d033bd07180048b8da0a6
Ethan Holter <holterethan@gmail.com>
2022-10-30 16:27:19 -0500

    help pt 2

--- Learning Materials/Inverse_Kinematics/inverse_kinematics.py
+++ Learning Materials/Inverse_Kinematics/inverse_kinematics.py
@@ -1,56 +1,69 @@
-from math import cos, pi, sin, sqrt
+import sys
+import time
 
-import py
 import pygame
+from pygame.math import Vector2
 
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
+
+points = list(map(Vector2, [(300, 300), (400, 300),
+              (500, 300), (540, 300)]))
+target = Vector2(450, 300)
+target_speed = Vector2(3, 3)
+
+rel_points = []
+angles = []
+
+max_angle = [360, 360, 120]  # Adjust for limited angles
+
+for i in range(1, len(points)):
+    rel_points.append(points[i] - points[i-1])
+    angles.append(0)
 
 
-def drawScreen():
-    screen.fill((255, 255, 255))
+def solve_ik(i, endpoint, target):
+    if i < len(points) - 2:
+        endpoint = solve_ik(i+1, endpoint, target)
+    current_point = points[i]
 
-    arm_len = 100
-    forearm_len = 100
-    shoulder_pos = (250, 450)
+    angle = (endpoint-current_point).angle_to(target-current_point)
+    angles[i] += angle
+    angles[i] = min(max(180 - max_angle[i], (angles[i]+180) %
+                    360), 180 + max_angle[i]) - 180
 
-    target = pygame.mouse.get_pos()
-    magnitude = sqrt((target[0] - shoulder_pos[0])**2
-                     + (target[1] - shoulder_pos[1])**2)
-
-    if magnitude > 200:
-        target = (200 * (target[0] - shoulder_pos[0])/magnitude + shoulder_pos[0],
-                  200 * (target[1] - shoulder_pos[1])/magnitude + shoulder_pos[1])
-
-    shoulder_angle = -pi/2
-    elbow_angle = -pi/2
-
-    elbow_pos = (arm_len*cos(shoulder_angle) +
-                 shoulder_pos[0], arm_len*sin(shoulder_angle) + shoulder_pos[1])
-
-    claw_pos = (forearm_len*cos(elbow_angle) +
-                elbow_pos[0], forearm_len*sin(elbow_angle) +
-                elbow_pos[1])
-
-    # draw all the stuff to the screen
-    pygame.draw.circle(screen, (0, 0, 255), target, 5)
-    pygame.draw.circle(screen, (0, 0, 0), shoulder_pos, 5)
-    pygame.draw.circle(screen, (0, 0, 0), elbow_pos, 5)
-    pygame.draw.circle(screen, (0, 0, 0), claw_pos, 5)
-    pygame.draw.line(screen, (0, 0, 0), shoulder_pos, elbow_pos, 2)
-    pygame.draw.line(screen, (0, 0, 0), elbow_pos, claw_pos, 2)
+    return current_point + (endpoint-current_point).rotate(angle)
 
 
-running = True
-while running:
+def render():
+    black = 0, 0, 0
+    white = 255, 255, 255
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    drawScreen()
+    screen.fill(white)
+    for i in range(1, len(points)):
+        prev = points[i-1]
+        cur = points[i]
+        pygame.draw.aaline(screen, black, prev, cur)
+    for point in points:
+        pygame.draw.circle(screen, black, (int(point[0]), int(point[1])), 5)
+    pygame.draw.circle(screen, black, (int(target[0]), int(target[1])), 10)
     pygame.display.flip()
 
 
-pygame.quit()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    target = pygame.mouse.get_pos()
+    solve_ik(0, points[-1], target)
+    angle = 0
+    for i in range(1, len(points)):
+        angle += angles[i-1]
+        points[i] = points[i-1] + rel_points[i-1].rotate(angle)
+
+    render()
+
+    pygame.time.wait(int(1000/60))
