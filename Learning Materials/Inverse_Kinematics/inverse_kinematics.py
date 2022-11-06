809c939622ba4c0e415eb65c52a084fe48350fc7
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2022-11-06 15:20:02 -0600

    updated file from main commit

--- Learning Materials/Inverse_Kinematics/inverse_kinematics.py
+++ Learning Materials/Inverse_Kinematics/inverse_kinematics.py
@@ -1,56 +1,125 @@
-from math import cos, pi, sin, sqrt
+import sys
 
-import py
 import pygame
+from pygame.math import Vector2
 
+# initialize pygame library and sub-modules
 pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+print("control with WASD")
 
-screen = pygame.display.set_mode([500, 500])
+# create GUI window pane
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
 
+# create a list of starting locations for each joint
+points = list(map(Vector2, [(100, 300), (200, 300), (300, 300)]))
 
-def drawScreen():
-    screen.fill((255, 255, 255))
+# define min and max angles each joint can move
+max_angles = [90, 0]
+min_angles = [-10, -120]
 
-    arm_len = 100
-    forearm_len = 100
-    shoulder_pos = (250, 450)
+# boundaries which end affector (target) is clamped to ((xlow, xhigh), (ylow, yhigh))
+target_bounds = ((101, 300), (200, 390))
 
-    target = pygame.mouse.get_pos()
-    magnitude = sqrt((target[0] - shoulder_pos[0])**2
-                     + (target[1] - shoulder_pos[1])**2)
-
-    if magnitude > 200:
-        target = (200 * (target[0] - shoulder_pos[0])/magnitude + shoulder_pos[0],
-                  200 * (target[1] - shoulder_pos[1])/magnitude + shoulder_pos[1])
-
-    shoulder_angle = -pi/2
-    elbow_angle = -pi/2
-
-    elbow_pos = (arm_len*cos(shoulder_angle) +
-                 shoulder_pos[0], arm_len*sin(shoulder_angle) + shoulder_pos[1])
-
-    claw_pos = (forearm_len*cos(elbow_angle) +
-                elbow_pos[0], forearm_len*sin(elbow_angle) +
-                elbow_pos[1])
-
-    # draw all the stuff to the screen
-    pygame.draw.circle(screen, (0, 0, 255), target, 5)
-    pygame.draw.circle(screen, (0, 0, 0), shoulder_pos, 5)
-    pygame.draw.circle(screen, (0, 0, 0), elbow_pos, 5)
-    pygame.draw.circle(screen, (0, 0, 0), claw_pos, 5)
-    pygame.draw.line(screen, (0, 0, 0), shoulder_pos, elbow_pos, 2)
-    pygame.draw.line(screen, (0, 0, 0), elbow_pos, claw_pos, 2)
+# calculate relative positions of each joint and populate angles array with placeholder values
+rel_points = []
+angles = []
+for i in range(1, len(points)):
+    rel_points.append(points[i] - points[i-1])
+    angles.append(0)
 
 
-running = True
-while running:
+def solve_ik(i, endpoint, target):  # recursively solve for the needed roation of each joint
+    if i < len(points) - 2:
+        endpoint = solve_ik(i+1, endpoint, target)
+    current_point = points[i]
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    angle = (endpoint-current_point).angle_to(target-current_point)
+    angles[i] += angle
 
-    drawScreen()
+    # honestly have no idea how this works
+    # limits the angles of each joint
+    angles[i] = min(max(180 - max_angles[i], (angles[i]+180) %
+                    360), 180 - min_angles[i]) - 180
+
+    return current_point + (endpoint-current_point).rotate(angle)
+
+
+def render():
+    black = 0, 0, 0
+    white = 255, 255, 255
+    blue = 0, 0, 255
+
+    screen.fill(white)
+
+    # draw the arm on screen
+    angle = 0
+    for i in range(1, len(points)):
+        angle += angles[i-1]
+        points[i] = points[i-1] + rel_points[i-1].rotate(angle)
+    for i in range(1, len(points)):
+        prev = points[i-1]
+        cur = points[i]
+        pygame.draw.aaline(screen, black, prev, cur)
+    for point in points:
+        pygame.draw.circle(screen, black, (int(point[0]), int(point[1])), 5)
+
+    # draw target
+    pygame.draw.circle(screen, blue,
+                       (int(target[0]), int(target[1])), 5)
+
+    # create debug text and draw to screen
+    angle1 = my_font.render(str(round(-angles[0], 3)), False, (0, 0, 0))
+    angle2 = my_font.render(str(round(-angles[1], 3)), False, (0, 0, 0))
+    coords = my_font.render(str(target), False, (0, 0, 0))
+    screen.blit(angle1, (20, 20))
+    screen.blit(angle2, (20, 40))
+    screen.blit(coords, (20, 60))
+
+    # update display
     pygame.display.flip()
 
 
-pygame.quit()
+n = 0
+target = (200, 305)
+while 1:
+
+    # check for x button clicked on GUI window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # move target with WASD
+    if pygame.key.get_pressed()[pygame.K_w]:
+        target = (target[0], target[1] - 1)
+    if pygame.key.get_pressed()[pygame.K_a]:
+        target = (target[0] - 1, target[1])
+    if pygame.key.get_pressed()[pygame.K_s]:
+        target = (target[0], target[1] + 1)
+    if pygame.key.get_pressed()[pygame.K_d]:
+        target = (target[0] + 1, target[1])
+
+    # limit target vector to vals specified in target_bounds
+    target = (min(max(target[0], target_bounds[0][0]),
+              target_bounds[0][1]), min(max(target[1], target_bounds[1][0]), target_bounds[1][1]))
+
+    # create circular left and right bounds
+    rel_target = Vector2(target) - points[0]
+    if rel_target.magnitude() > 190:
+        rel_target.scale_to_length(190)
+        rel_target = (points[0] + rel_target)
+        target = (int(rel_target.x), int(rel_target.y))
+    elif rel_target.magnitude() < 100:
+        rel_target.scale_to_length(100)
+        rel_target = (points[0] + rel_target)
+        target = (int(rel_target.x), int(rel_target.y))
+
+    # use inverse kinematics algorithm
+    solve_ik(0, points[-1], target)
+
+    # update display
+    render()
+
+    pygame.time.wait(int(1000/60))
