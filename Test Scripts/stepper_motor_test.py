7b774103575516a0e545d214c08ba0d37ac5185f
Ethan Holter <holterethan@gmail.com>
2022-11-09 20:44:49 -0600

    moved files to different branch

--- Test Scripts/stepper_motor_test.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-import inverse_kinematics
-
-ik = threading.Thread(target=inverse_kinematics.start_IK, daemon=True)
-ik.start()
-
-while True:
-    print(inverse_kinematics.angles)
